b7b5680388e4663840851725eacc82d2632e3492
marcello <marcello@marcello-p6231it>
2016-06-21 14:09:38 +0200

    .

--- wolf/bin/build_agent.sh
+++ wolf/bin/build_agent.sh
@@ -4,12 +4,17 @@ echo "BUILDING AGENT"
 
 export CURPATH="$(dirname $(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}"))"
 export BASEDIR=$CURPATH/..
-export MODULE=agent
 
 . $CURPATH/_setenv.sh
 
-cd $BASEDIR/$MODULE
-$CURPATH/_mvn2.sh clean package
+cd $BASEDIR
+$CURPATH/_mvn2.sh install
+
+cd $BASEDIR/adapter
+$CURPATH/_mvn2.sh clean install
+
+cd $BASEDIR/agent
+$CURPATH/_mvn2.sh clean package assembly:assembly
 
 echo ""
 read -p "Premere un tasto per continuare"
